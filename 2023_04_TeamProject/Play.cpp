0aae09461e6428b06264624b6bb19ebd49f86f30
Melolololon <75592470+Melolololon@users.noreply.github.com>
2023-05-12 16:34:44 +0900

    ObjectManagerの処理追加

--- 2023_04_TeamProject/Play.cpp
+++ 2023_04_TeamProject/Play.cpp
@@ -4,14 +4,18 @@
 
 void Play::Initialize()
 {
+	// オブジェクトのInitializeを呼び出す
+	//MelLib::GameObjectManager::GetInstance()->InitializeObject();
 }
 
 void Play::Update()
 {
+	MelLib::GameObjectManager::GetInstance()->Update();
 }
 
 void Play::Draw()
 {
+	MelLib::GameObjectManager::GetInstance()->Draw();
 }
 
 void Play::Finalize()
